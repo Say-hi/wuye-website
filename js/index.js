cb2e0bdeeada30cbbd5ff6a54e9f337723f91bdd
江文强 <782108446@qq.com>
2017-03-20 11:43:51 +0800

    '前台页面搭建完毕'

--- js/index.js
+++ js/index.js
@@ -20,12 +20,39 @@ $(function(){
     //loan 页 choose切换
     var $loantabs = $('.c-t-li');
     $('.c-t-li').on("click",function(){
+        $('.choose').addClass('go-to-top');
         for (var i = 0; i < $loantabs.length; i++) {
-            $($loantabs[i]).removeClass('active')
+            $($loantabs[i]).removeClass('active');
+            $('.c-t-l').removeClass('active');
         }
-        $(this).addClass("active");
-    })
 
+        $(this).addClass("active");
+        var id = $(this).attr('data-tab').slice(-1);
+        console.log(id);
+        $('.c-t-l').eq(id).addClass('active');
+        $('.choose-popup').addClass('show');
+        $('.mask').addClass('show');
+    })
+    // 借款条件切换
+    $('.c-t-l-li').on('click',function(){
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+    // 重置
+    $('.btn-cancel').on('click',function(){
+        $('.c-t-l-li').removeClass('active');
+    })
+    // 确定
+    $('.btn-confirm').on('click',function(){
+        $('.choose-popup').removeClass('show');
+        $('.mask').removeClass('show');
+        $('.choose').removeClass('go-to-top');
+    })
+    // 点击遮罩层
+    $('.mask').on('click',function(){
+        $('.choose-popup').removeClass('show');
+        $('.mask').removeClass('show');
+        $('.choose').removeClass('go-to-top');
+    })
     // bbs tab切换
     var $bbstabs = $('.bbs-t');
     var $articles= $('.article')
